1fa0cb101e7ee00582f7c7f0aa71dc38e4f34dab
Ghulam89 <gm6681328@gmail.com>
2025-06-11 09:40:13 -0700

    update3

--- app.js
+++ app.js
@@ -342,9 +342,9 @@ app.use("/requestQuote", requestQuoteRouter);
 app.use("/instantQuote", instantQuoteRouter);
 
 
-// app.get("/apis", async (req, res) => {
-//   res.send("App Is Running");
-// });
+app.get("/apis", async (req, res) => {
+  res.send("App Is Running");
+});
 
 
 // app.get("/apis", async (req, res) => {
