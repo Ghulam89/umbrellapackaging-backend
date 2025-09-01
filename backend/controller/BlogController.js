24c2740e157219514c0c1ae35e2a630088861a4c
Ghulam89 <gm6681328@gmail.com>
2025-09-01 05:36:33 -0700

    first commit

--- backend/controller/BlogController.js
+++ backend/controller/BlogController.js
@@ -293,7 +293,6 @@ export const getAllBlogs = catchAsyncError(async (req, res, next) => {
     }
     const [blogs, totalBlogs] = await Promise.all([
       Blogs.find(filter)
-        .select("slug title image imageAltText metaTitle metaDescription createdAt keywords")
         .sort({ createdAt: -1 })
         .skip(skip)
         .limit(limit)
