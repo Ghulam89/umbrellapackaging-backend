2418afdfa0d45996ccec994b1d533543e268a7c5
Ghulam89 <gm6681328@gmail.com>
2025-09-12 13:06:55 -0700

    update

--- frontend/src/pages/productDetails/index.jsx
+++ frontend/src/pages/productDetails/index.jsx
@@ -947,7 +947,7 @@ const productSchema = {
              {product?.images?.[0]?.url && (
   <img
     className="rounded-xl w-full h-auto object-cover shadow-md"
-    src={`${BaseUrl}/${product.images[0].url}`}
+    src={`${BaseUrl}/${product?.images[0]?.url}`}
     alt="Automobile Tuck End Boxes"
   />
 )}
