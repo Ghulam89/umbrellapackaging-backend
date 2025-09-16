a72eb5273cf4f9f7d449d5ed2063b4b15bb4bd3c
Ghulam89 <gm6681328@gmail.com>
2025-09-16 07:43:01 -0700

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react({
-      fastRefresh: false,
-    }),
+    react(),
   ],
 
   resolve: {
