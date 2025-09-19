8f7c32a7590a9b9890b6c247b8fddb3ad010db48
Ghulam89 <gm6681328@gmail.com>
2025-09-19 08:34:51 -0700

    first commit

--- frontend/src/components/common/CategoryCard.jsx
+++ frontend/src/components/common/CategoryCard.jsx
@@ -18,10 +18,6 @@ const CategoryCard = ({ data }) => {
             src={data?.image ? `${BaseUrl}/${data.image}` : ""}
             alt={altText}
             className="w-full sm:h-62 h-auto object-cover overflow-hidden rounded-lg"
-            loading="lazy"
-            onError={(e) => {
-              e.currentTarget.src = "/placeholder.png";
-            }}
           />
         </div>
         <h2 className="sm:text-base text-sm font-semibold text-[#333333] uppercase sm:py-5 py-2">
