e411d09bb438a19535d98ccf7c00b0c2fe96fc78
Ghulam89 <gm6681328@gmail.com>
2025-11-12 06:06:43 -0800

    update

--- frontend/src/components/common/CardSlider.jsx
+++ frontend/src/components/common/CardSlider.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
@@ -7,12 +6,16 @@ import "swiper/css/pagination";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import ProductCard from "./ProductCard";
+
 const CardSlider = ({ item, index }) => {
   const [isAutoPlay, setIsAutoPlay] = useState(true);
+
   const handleScroll = () => {
     const scrollPosition = window.scrollY;
-    setIsAutoPlay(scrollPosition <= 100);
+  
+    setIsAutoPlay(scrollPosition < 50); 
   };
+
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => {
@@ -22,46 +25,54 @@ const CardSlider = ({ item, index }) => {
 
   return (
     <div className="">
-      <div className="w-full  mx-auto relative">
-
-    <Swiper
-  modules={[Autoplay, Pagination, Navigation]}
-  autoplay={
-    isAutoPlay ? { delay: 3000, disableOnInteraction: false } : false
-  }
-  loop={true}
-  spaceBetween={30}
-  slidesPerView="auto"
-  breakpoints={{
-    640: { slidesPerView: 2 },
-    768: { slidesPerView: 3 },
-    1024: { slidesPerView: 4 },
-  }}
-  onBeforeInit={(swiper) => {
-    swiper.params.navigation.prevEl = `.custom-prev-${index}`;
-    swiper.params.navigation.nextEl = `.custom-next-${index}`;
-  }}
-  navigation={{
-    nextEl: `.custom-next-${index}`,
-    prevEl: `.custom-prev-${index}`,
-  }}
->
-  {item?.map((item) => (
-    <SwiperSlide key={item.id}>
-      <div className="bg-[#f7f7f7] p-2 rounded-xl max-w-6xl mx-auto">
-        <ProductCard data={item} />
-      </div>
-    </SwiperSlide>
-  ))}
-</Swiper>
-
-
+      <div className="w-full mx-auto relative">
+        <Swiper
+          modules={[Autoplay, Pagination, Navigation]}
+          autoplay={
+            isAutoPlay ? { 
+              delay: 3000, 
+              disableOnInteraction: false,
+              pauseOnMouseEnter: true 
+            } : false
+          }
+          loop={true}
+          spaceBetween={30}
+          slidesPerView="auto"
+          breakpoints={{
+            640: { slidesPerView: 2 },
+            768: { slidesPerView: 3 },
+            1024: { slidesPerView: 4 },
+          }}
+          onBeforeInit={(swiper) => {
+            swiper.params.navigation.prevEl = `.custom-prev-${index}`;
+            swiper.params.navigation.nextEl = `.custom-next-${index}`;
+          }}
+          navigation={{
+            nextEl: `.custom-next-${index}`,
+            prevEl: `.custom-prev-${index}`,
+          }}
+         
+          speed={800}
+          grabCursor={true}
+        >
+          {item?.map((item) => (
+            <SwiperSlide key={item.id}>
+              <div className="bg-[#f7f7f7] p-2 rounded-xl max-w-6xl mx-auto">
+                <ProductCard data={item} />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
 
         <div className="flex w-full justify-center sm:mt-0 mt-6 gap-3 items-center">
-          <button className={`custom-prev-${index} md:absolute top-[40%] z-40 md:-left-15 w-10 h-10 bg-[#F6F6F6] text-[#4440E6] hover:bg-[#4440E6] hover:text-white rounded-xl flex items-center justify-center`}>
+          <button 
+            className={`custom-prev-${index} md:absolute top-[40%] z-40 md:-left-15 w-10 h-10 bg-[#F6F6F6] text-[#4440E6] hover:bg-[#4440E6] hover:text-white rounded-xl flex items-center justify-center`}
+          >
             <IoIosArrowBack size={25} />
           </button>
-          <button className={`custom-next-${index} w-10 h-10 md:absolute top-[40%] z-40 md:-right-15 bg-[#F6F6F6] text-[#4440E6] hover:bg-[#4440E6] hover:text-white rounded-xl flex items-center justify-center`}>
+          <button 
+            className={`custom-next-${index} w-10 h-10 md:absolute top-[40%] z-40 md:-right-15 bg-[#F6F6F6] text-[#4440E6] hover:bg-[#4440E6] hover:text-white rounded-xl flex items-center justify-center`}
+          >
             <IoIosArrowForward size={25} />
           </button>
         </div>
